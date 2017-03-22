dffe922d9df344f7b3ab37824b83e721b75bcd19
Dawood Ahmed <dawoodahmed@Dipeshs-MacBook-Pro.local>
2017-03-22 07:03:06 +0530

    front end changes

---
+++ platforms/browser/www/js/my-app.js
@@ -0,0 +1,226 @@
+var base_url = 'http://kreaserv-tech.com/mahindra_admin/index.php/api';
+var email_regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+var token = {};
+
+var myApp = new Framework7({
+
+    pushState: true,
+    // swipePanel: 'right',
+    swipeBackPage:false,
+    preloadPreviousPage: false,
+    uniqueHistory: true,
+    uniqueHistoryIgnoreGetParameters: true,
+    modalTitle: 'Powerol',
+    imagesLazyLoadPlaceholder: 'img/lazyload.jpg',
+    imagesLazyLoadThreshold: 50,
+
+});
+
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+	// dynamicNavbar: true
+});
+
+mainView.hideNavbar();
+
+
+var token = Lockr.get('token');
+
+myApp.onPageInit('about', function (page) {
+
+})
+
+$$(document).on('pageInit', function (e) {
+
+
+    console.log("page init called");
+
+
+    var page = e.detail.page;
+    if (page.name === 'about'){
+    	
+    	myApp.alert("this is about page");
+    }
+})
+
+myApp.onPageInit('findus', function(page) {
+});
+
+myApp.onPageInit('findus_map', function(page) {
+
+});
+
+myApp.onPageInit('findus_dealer', function(page) {
+   
+});
+
+myApp.onPageInit('book_now', function(page) {
+});
+
+myApp.onPageInit('video', function(page) {
+    // var myPhotoBrowserPopupDark = myApp.photoBrowser({
+    // photos : [
+    //     {
+    //         html: '<iframe width="100%" height="215" src="http://www.youtube.com/embed/lmc21V-zBq0?list=PLpj0FBQgLGEr3mtZ5BTwtmSwF1dkPrPRM?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>',
+    //     },
+    // ],
+    // theme: 'dark',
+    // type: 'standalone'
+    // });
+    // $('.video_button_view_profile').on('click', function () {
+    //      myPhotoBrowserPopupDark.open();
+    // });
+});
+
+myApp.onPageInit('enquiry_form', function(page) {
+    $("#tab1_button").css("background-color", "#3c73c0");
+    $("#tab1_button").css("color", "#fff");
+
+    $('#tab1_button').on('click', function () {
+        $("#tab1_button").css("background-color", "#3c73c0");
+        $("#tab1_button").css("color", "#fff");
+        $("#tab2_button").css("background-color", "");
+        $("#tab2_button").css("color", "#3c73c0");
+        myApp.showTab('#tab1');
+    });
+ 
+    $('#tab2_button').on('click', function () {
+        myApp.showTab('#tab2');
+        $("#tab2_button").css("background-color", "#3c73c0");
+        $("#tab2_button").css("color", "#fff");
+        $("#tab1_button").css("background-color", "");
+        $("#tab1_button").css("color", "#3c73c0");
+    });
+});
+
+myApp.onPageInit('our_story', function(page) {
+    // var myPhotoBrowserPopupDark = myApp.photoBrowser({
+    //     photos : [
+    //         {
+    //             html: '<iframe src="https://onedrive.live.com/embed?cid=060AC7C6A4465D4A&resid=60AC7C6A4465D4A%21112&authkey=AB6qsUMz2m9nWoQ&em=2" width="402" height="327" frameborder="0" scrolling="no"></iframe>',
+    //         },
+    //     ],
+    //     theme: 'dark',
+    //     type: 'standalone'
+    //     });
+    // $('.our_story_button_view_profile').on('click', function () {
+    //     window.open('')
+    // });
+});
+
+myApp.onPageInit('inside_outside', function(page) {
+    $("#tab1_button").css("background-color", "#0173bc");
+    $("#tab1_button").css("color", "#fff");
+    $("#tab1_button").css("border", "2px solid #0173bc");
+
+    $('#tab1_button').on('click', function () {
+         myApp.showTab('#tab1');
+        $("#tab1_button").css("background-color", "#0173bc");
+        $("#tab1_button").css("color", "#fff");
+        $("#tab1_button").css("border", "2px solid #0173bc");
+        $("#tab2_button").css("background-color", "");
+        $("#tab2_button").css("color", "#fff");
+        $("#tab2_button").css("border", "2px solid #fff");
+    });
+ 
+    $('#tab2_button').on('click', function () {
+        myApp.showTab('#tab2');
+        $("#tab2_button").css("background-color", "#0173bc");
+        $("#tab2_button").css("color", "#fff");
+        $("#tab2_button").css("border", "2px solid #0173bc");
+        $("#tab1_button").css("background-color", "");
+        $("#tab1_button").css("color", "#fff");
+        $("#tab1_button").css("border", "2px solid #fff");
+    });
+});
+
+myApp.onPageInit('faq', function(page) {
+    $("#tab1_button").css("background-color", "#0173bc");
+    $("#tab1_button").css("color", "#fff");
+
+    $('#tab1_button').on('click', function () {
+        $("#tab1_button").css("background-color", "#0173bc");
+        $("#tab1_button").css("color", "#fff");
+        $("#tab2_button,#tab3_button,#tab4_button,#tab5_button").css("background-color", "");
+        $("#tab2_button,#tab3_button,#tab4_button,#tab5_button").css("color", "#3c73c0");
+        myApp.showTab('#tab1');
+    });
+ 
+    $('#tab2_button').on('click', function () {
+        myApp.showTab('#tab2');
+        $("#tab2_button").css("background-color", "#0173bc");
+        $("#tab2_button").css("color", "#fff");
+        $("#tab1_button,#tab3_button,#tab4_button,#tab5_button").css("background-color", "");
+        $("#tab1_button,#tab3_button,#tab4_button,#tab5_button").css("color", "#3c73c0");
+    });
+
+    $('#tab3_button').on('click', function () {
+        myApp.showTab('#tab3');
+        $("#tab3_button").css("background-color", "#0173bc");
+        $("#tab3_button").css("color", "#fff");
+        $("#tab1_button,#tab2_button,#tab4_button,#tab5_button").css("background-color", "");
+        $("#tab1_button,#tab2_button,#tab4_button,#tab5_button").css("color", "#3c73c0");
+    });
+
+
+    $('#tab4_button').on('click', function () {
+        myApp.showTab('#tab4');
+        $("#tab4_button").css("background-color", "#0173bc");
+        $("#tab4_button").css("color", "#fff");
+        $("#tab1_button,#tab3_button,#tab2_button,#tab5_button").css("background-color", "");
+        $("#tab1_button,#tab3_button,#tab2_button,#tab5_button").css("color", "#3c73c0");
+    });
+
+    $('#tab5_button').on('click', function () {
+        myApp.showTab('#tab5');
+        $("#tab5_button").css("background-color", "#0173bc");
+        $("#tab5_button").css("color", "#fff");
+        $("#tab1_button,#tab3_button,#tab2_button,#tab4_button").css("background-color", "");
+        $("#tab1_button,#tab3_button,#tab2_button,#tab4_button").css("color", "#3c73c0");
+    });
+
+
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].onclick = function() {
+        this.classList.toggle("active");
+        var accordion_content = this.nextElementSibling;
+        if (accordion_content.style.maxHeight){
+          accordion_content.style.maxHeight = null;
+        } else {
+          accordion_content.style.maxHeight = accordion_content.scrollHeight + "px";
+        } 
+      }
+    }
+});
+
+myApp.onPageInit('5k_punch', function(page) {
+
+    $('img').click(function(){
+
+        $("img").removeClass('zoomin');
+        $("img").removeClass('show');
+        $('.innerp').css("display","none");
+
+        $(this).addClass('zoomin');
+        $(this).addClass('show');
+
+        var cls = $(this).context.className.split(' ')[0]+"Content";
+        $('.'+cls).find('.innerp').css("display","block");
+
+    })
+   
+});
+
+myApp.onPageInit('home', function(page) {
+    $(".owl-carousel").owlCarousel();
+});
+
+$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
+
+})
---
+++ platforms/browser/www/js/util.js
@@ -0,0 +1,141 @@
+
+function j2s(json) {
+    return JSON.stringify(json);
+}
+
+function goto_page(page) {
+    mainView.router.load({
+        url: page,
+        ignoreCache: false,
+    });
+}
+
+
+//app login
+function login(){
+	var email = $('#login-email').val();
+    var password = $('#login-password').val();
+    if (email == '') {
+        myApp.alert('Email Id should be provided.');
+        return false;
+    } else if (!email.match(email_regex)) {
+        myApp.alert('Valid Email Id should be provided.');
+        return false;
+    }
+
+    if (password == '') {
+        myApp.alert('Password should not be blank.');
+        return false;
+    }
+
+    myApp.showIndicator();
+    $.ajax({
+        url: base_url + '/login',
+        type: 'POST',
+        crossDomain: true,
+        data: {
+            "email": email,
+            "password": password,
+        },
+    })
+    .done(function(res) {
+        console.log('done: ' + j2s(res));
+        myApp.hideIndicator();
+        if (res.status == 'SUCCESS') {
+            mainView.router.load({
+                url: 'home.html',
+                ignoreCache: false,
+            });
+        } else {
+            myApp.alert('Email or Password Mismatch');
+        }
+    })
+    .fail(function(err) {
+        myApp.hideIndicator();
+        myApp.alert('Some error occurred on connecting.');
+        console.log('fail: ' + j2s(err));
+    })
+    .always(function() {});
+}
+
+function logout() {
+    Lockr.flush();
+    token = false;
+    goto_page('index.html');
+}
+
+
+
+    // $(document).ready(function(){
+
+    //     alert('clicked');
+    //     $('img').click(function(){
+    //     alert('clicked');
+
+
+    //         $("img").removeClass('zoomin');
+    //         $("img").removeClass('show');
+
+    //         $(this).addClass('zoomin');
+    //         $(this).addClass('show');
+
+    //     })
+        
+    // })
+
+
+// alert('helo');
+
+
+// function initMap() {
+//     // Create a map object and specify the DOM element for display.
+//     var map = new google.maps.Map(document.getElementById('map'), {
+//       center: {lat: -34.397, lng: 150.644},
+//       scrollwheel: false,
+//       zoom: 8
+//     });
+//   }
+
+//back button
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+
+    console.log("device ready called");
+
+
+
+
+
+    document.addEventListener("backbutton", function(e) {
+        e.preventDefault();
+        var page = myApp.getCurrentView().activePage;
+        myApp.hideIndicator();
+         myApp.closePanel();
+        if (page.name == "home"  || page.name == "index" ) {
+            // lockFile = dataDir.getFile("file:///lockfile.txt", {create: true, exclusive: true});
+            // console.log("Created File"+lockFile);
+            myApp.confirm('would you like to exit app.', function() {
+                navigator.app.clearHistory();
+                gaPlugin.exit(nativePluginResultHandler, nativePluginErrorHandler);
+                navigator.app.exitApp();
+            });
+        } else {
+            mainView.router.back({});
+        }
+    }, false);
+}
+
+function nativePluginResultHandler(result) {
+    // console.log('GA result: '+result);
+    // alert('GA result: '+result);
+}
+
+function nativePluginErrorHandler(error) {
+    // console.log('GA error: '+error);
+    // alert('GA error: '+error);
+}
+
+function inside_outside_text(){
+    $('.inside_outside_text').show();
+}
